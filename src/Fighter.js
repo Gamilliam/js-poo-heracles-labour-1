3a424f22915aa2c278eeee0449038ae290e5a543
Gamilliam <brnlcecile@gmail.com>
2023-07-19 15:16:46 +0200

    Updated console messages

--- src/Fighter.js
+++ src/Fighter.js
@@ -2,47 +2,42 @@
 
 const MAX_LIFE = 100;
 class Fighter {
-  constructor(name, strength, dexterity) {
-    this.name = name;
-    this.strength = strength;
-    this.dexterity = dexterity;
-    this.life = MAX_LIFE;
-  };
+    constructor(name, strength, dexterity) {
+        this.name = name;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.life = MAX_LIFE;
+    };
 
-  fight(defender) {
-    const attack = this.strength;
-    let attackValue = Math.floor(Math.random() * attack);
-    if (attackValue <= 0) {
-      attackValue = 1;
+    fight(defender) {
+        const attack = this.strength;
+        let attackValue = (Math.floor(Math.random() * attack)) - defender.dexterity;
+        if (attackValue <= 0) {
+            attackValue = 1;
+        }
+
+        console.log(
+            this.name,
+            "🗡️  attacks \u{1F6E1} ",
+            defender.name,
+            "(",
+            defender.life, "💙)"
+        );
+
+        defender.life = defender.life - attackValue;
+        if (defender.life <= 0) {
+            defender.life = 0;
+        }
+
+        return defender.life;
+    };
+
+    isAlive() {
+        if (this.life) {
+            return true;
+        } else {
+            return false;
+        }
     }
-
-    let attackFinal = attackValue - defender.dexterity;
-    if (attackFinal <= 0) {
-      attackFinal = 1;
-    }
-
-    console.log(
-      this.name,
-      "🗡️ ",
-      defender.name,
-      "(",
-      defender.life, "💙)"
-    );
-
-    defender.life = defender.life - attackFinal;
-    if (defender.life <= 0) {
-      defender.life = 0;
-    }
-
-    return defender.life;
-  };
-
-  isAlive() {
-    if (this.life) {
-      return true;
-    } else {
-      return false;
-    }
-  }
 }
 module.exports = Fighter;
