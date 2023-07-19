fcf23562b8d30742d859fe68d7d3a9ffb94bc1c4
cedric candes <candaescedric63@gmail.com>
2023-07-19 12:39:29 +0200

    heracles vs the pebble (and a lion) in js

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+const Fighter = require("./src/Fighter.js");
+
+const Heracles = new Fighter("🧔 Heracles", 20, 6);
+const Lion = new Fighter("🦁 Nemean Lion", 11, 13);
+const Pebble = new Fighter("Petit caillou, The True French Pebble", 1, 1);
+
+console.log(
+  "Héraclès s’échauffe un peu, resserre ses muscles, écrase quelques rochers à mains nues. Il est prêt à se battre !\n Héraclès entre dans la grotte, le Lion est là, ses yeux rouges pointés sur son ennemi. Le combat est imminent :"
+);
+
+let round = 0;
+while (Heracles.isAlive() && Lion.isAlive()) {
+  round++;
+  console.log("\n🕛 Round:", round);
+  Heracles.fight(Lion);
+  if (!Lion.isAlive()) {
+    break;
+  }
+  Lion.fight(Heracles);
+}
+
+if (Lion.life === 0) {
+  console.warn("💀 ", Lion.name, "is dead");
+    console.warn("🏆 ", Heracles.name, "wins with", Heracles.life + " point(s) of life");
+    console.log(Heracles.name + " walks away from the battle when he steps on a pebble on the road...");
+    Pebble.fight(Heracles);
+    if(Heracles.life===0){
+        console.log(Pebble.name, "wins. Nature is the greatest warrior of all.");
+    } else {
+    console.warn("🏆 ", Heracles.name, "is the great winner with", Heracles.life + " point(s) of life");
+    }
+} else {
+  console.warn("💀 ", Heracles.name, "is dead");
+  console.warn("🏆 ", Lion.name, "with", Lion.life +"point(s) of life");
+}
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,48 @@
 /* Fighter class definition */
+
+const MAX_LIFE = 100;
+class Fighter {
+  constructor(name, strength, dexterity) {
+    this.name = name;
+    this.strength = strength;
+    this.dexterity = dexterity;
+    this.life = MAX_LIFE;
+  };
+
+  fight(defender) {
+    const attack = this.strength;
+    let attackValue = Math.floor(Math.random() * attack);
+    if (attackValue <= 0) {
+      attackValue = 1;
+    }
+
+    let attackFinal = attackValue - defender.dexterity;
+    if (attackFinal <= 0) {
+      attackFinal = 1;
+    }
+
+    console.log(
+      this.name,
+      "🗡️ ",
+      defender.name,
+      "(",
+      defender.life, "💙)"
+    );
+
+    defender.life = defender.life - attackFinal;
+    if (defender.life <= 0) {
+      defender.life = 0;
+    }
+
+    return defender.life;
+  };
+
+  isAlive() {
+    if (this.life) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+module.exports = Fighter;
